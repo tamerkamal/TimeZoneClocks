54cd48c925eb4385bf9b305189db4e393f0ce2b2
Tamer Kamal Abdelsalam <infinite.group7@outlook.com>
2022-02-26 20:24:45 +0200

    first commit

---
+++ src/clocks.js
@@ -0,0 +1,30 @@
+window.onload = () => {
+    displayCLocks();
+    setInterval(displayCLocks, 1000);
+}
+
+function getCurrentTime(nationality, timeZone,) {
+
+    let currentTime = new Date().toLocaleString('en-us',
+        { timeZone: timeZone, timeStyle: 'medium', hourCycle: 'h12' }
+    );
+
+    console.log(nationality + ': ', currentTime);
+
+    return currentTime;
+}
+
+function displayCLocks() {
+
+    let dutchTime = getCurrentTime('Dutch', 'Europe/Amsterdam');
+    let egyptTime = getCurrentTime('Egyptian', 'Africa/Cairo');
+    let indianTime = getCurrentTime('Indian', 'Asia/Kolkata');
+
+    document.getElementById('dutchTZone').innerHTML = dutchTime;
+    document.getElementById('egyptTZone').innerHTML = egyptTime;
+    document.getElementById('indiaTZone').innerHTML = indianTime;
+}
+
+
+
+
